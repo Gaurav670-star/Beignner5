d10f2d25572a0310decc3ca3bd00425240fe5557
Gaurav670-star <85485197+Gaurav670-star@users.noreply.github.com>
2021-06-10 09:33:59 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person{
+
+private int age;
+private String name;
+public void setAge(int a){
+   age = a;
+}
+public void setName(String a){
+name = a;
+}
+public int getAge(){
+
+ return(a);
+}
+public String getName(){
+return(name);
+}
+}
